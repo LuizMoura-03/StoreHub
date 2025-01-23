094c19a12f6eecb21895ee7302f4fcda61d22cb5
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-23 15:21:44 -0300

    feat: adiconando metodo getProductById para buscar produto por ID

--- src/main/java/com/exercicio/StoreHub/services/ProductoService.java
+++ src/main/java/com/exercicio/StoreHub/services/ProductoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductoService {
@@ -13,10 +14,12 @@ public class ProductoService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<ProductModel> getAllProductos() {
+    public List<ProductModel> getAllProducts() {
         return productRepository.findAll();
     }
 
+    public Optional<ProductModel> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
     
-
 }
